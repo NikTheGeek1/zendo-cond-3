411b3ccb9407d7508629d5f32a1d10973fe2a6ab
xcovers <ntheodoropoulos@outlook.com>
2020-05-30 19:59:16 +0300

    updates

--- server.js
+++ server.js
@@ -90,3 +90,8 @@ const port = process.env.PORT || 5000;
 app.listen(port);
 
 console.log('App is listening on port ' + port);
+
+// Handles any requests that don't match the ones above
+app.get('*', (req,res) =>{
+    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
